e791f47295dc15369bc19c146aaa47db59806bc3
Khalid Husain <khusain444@gmail.com>
2018-12-31 03:34:13 -0500

    more fixes

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api', require('./api'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
