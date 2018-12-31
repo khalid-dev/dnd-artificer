0a1f6db4b8ed15b78311837e65b4bb3f54c94b60
khusain <judicatorkhalid@gmail.com>
2018-12-30 23:37:57 -0500

    netlify script

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 'use strict'
 const app = require('./server')
 const PORT = process.env.PORT || 3000
+require('dotenv').config();
+
+console.log(process.env.NODE_ENV);
  
 app.listen(PORT, () => console.log(`App running on Port: ${PORT}`))
